a4bb871057ae9f4637c4accdd54c86bd7d494087
Mundia <fxmundia416@gmail.com>
2021-04-14 11:35:05 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const context = require("./server/context");
-const userResolvers = require("./server/resolvers/userResolver");
-const secondaryUserResolvers = require("./server/resolvers/secondaryUserResolver");
 const AuthProvider = require("./client/components/auth-provider")
-const userTypeDef = require("./server/typedefs/user");
 const useAuth = require("./client/hooks/useAuth")
 
 
@@ -23,17 +19,6 @@ BASE_URL=
  */
 
 module.exports = {
-    //1. add the context object to ApolloServer object
-    context,
-    //2. add the userResolver to the resolvers index
-    userResolvers,
-    //3. add the userTypeDef to the typedefs index
-    userTypeDef,
-    //4.[optional] add the secondaryUserResolver to the resolvers index if using secondary logins
-    secondaryUserResolvers,
-
-
-
     //the client side api
     useAuth,
     //wrap the components in the src/index file with AuthProvider
--- server/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ERROR: 0,
-    SUCCESS: 1,
-    NOT_LOGGED_IN: 2,
-    EMAIL_NON_EXIST: 3,
-    USER_ALREADY_EXISTS: 5,
-    WRONG_PASSWORD: 9
-}
